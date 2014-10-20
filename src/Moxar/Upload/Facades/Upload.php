41e9b38dbef6803c235485fd4f0888170eb62146
Alexandre Thomas <alexandre.thomas@outlook.fr>
2014-10-20 22:31:30 +0200

    Bugfixes in folder organisation.

--- src/Moxar/Upload/Facades/Upload.php
+++ src/Moxar/Upload/Facades/Upload.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Facade;
 
 class Upload extends Facade {
 
-    protected static function getFacadeAccessor() { 
+    protected static function getFacadeAccessor() {
         return 'upload'; 
     }
 }
---
+++ src/Moxar/Upload/UploadServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Moxar\Upload;
+
+use Illuminate\Support\ServiceProvider;
+
+class UploadServiceProvider extends ServiceProvider {
+
+    protected $defer = true;
+
+//     public function register() {
+//         $this->app->bind('upload', function()
+//         {
+//             return new Upload;
+//         });
+//     }
+
+    public function boot() {
+            $this->package('moxar/upload');
+    }
+
+    public function register()
+    {
+        $this->app['upload'] = $this->app->share(function($app) {
+            return new Upload($app);
+        });
+        $this->app->booting(function() {
+            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+            $loader->alias('Upload', 'Moxar\Upload\Facades\Upload');
+        });
+    }
+
+    public function provides() {
+            return array('upload');
+    }
+}
